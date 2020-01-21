0eb66464a078d55fb039b3cd4213ffec45fc8a2b
louisaliu23 <47044375+louisaliu23@users.noreply.github.com>
2020-01-20 21:57:00 -0800

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://c52f4d9c.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://15e81097.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
